ff82973261bd19a6da05ed19e7b35234a19d81e7
W4zt3r <44176287+bribrizoo@users.noreply.github.com>
2023-01-06 22:17:36 +0100

    Vendors
    Python script to get all vendors from Wikipedia

---
+++ Vendors.py
@@ -0,0 +1,15 @@
+import requests
+import os
+import urllib.request
+from bs4 import BeautifulSoup
+
+URL = "https://fr.wikipedia.org/wiki/Cat%C3%A9gorie:Fabricant_d%27armes_%C3%A0_feu"
+page = requests.get(URL)
+if page.status_code == 200 :
+    #Récupération des différentes catégories d'armes
+    soup = BeautifulSoup(page.content, 'html.parser')
+    #Liste des balise 'a' de la 'navbar'
+    vendors = soup.select(".mw-category ul li")
+    f = open("Fabricants.txt", 'a')
+    for vendor in vendors :
+        f.write(vendor.get_text())
